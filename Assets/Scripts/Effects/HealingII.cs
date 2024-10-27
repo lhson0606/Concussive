d71e252a3e3648498062cc2d8275a80ea8e2da16
lhson0606 <lehoangson01633892497@gmail.com>
2024-10-27 09:02:33 +0700

    add heal potion

---
+++ Assets/Scripts/Effects/HealingII.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HealingII : Effect
+{
+    [SerializeField]
+    int healPerTick = 1;
+
+    public override void ApplyEffect()
+    {
+        base.ApplyEffect();
+        target.Heal(healPerTick);
+    }
+}
