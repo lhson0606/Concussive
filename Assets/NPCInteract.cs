b8e7cfdeba0a10e25b307d2493b4391ac9a47d54
20120627 <vinhtenbivn@gmail.com>
2024-12-19 22:48:51 +0700

    Basic shop function

---
+++ Assets/NPCInteract.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class NPCInteract : MonoBehaviour
+{
+    [SerializeField] private GameObject shopUIPanel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            OpenShop();
+    }
+
+    private void OpenShop()
+    {
+        ShopMenu shopMenu = shopUIPanel.GetComponent<ShopMenu>();
+        if (shopMenu != null)
+        {
+            shopMenu.Shopping();
+        }
+        else
+        {
+            Debug.LogError("ShopMenu component not found on shopUIPanel");
+        }
+    }
+
+    private void CloseShop()
+    {
+        ShopMenu shopMenu = shopUIPanel.GetComponent<ShopMenu>();
+        if (shopMenu != null)
+        {
+            shopMenu.Exit();
+        }
+        else
+        {
+            Debug.LogError("ShopMenu component not found on shopUIPanel");
+        }
+    }
+}
---
+++ Assets/ShopItem.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopItem : MonoBehaviour
+{
+    [SerializeField] private GameObject ItemImage;
+    [SerializeField] private GameObject ItemName;
+    [SerializeField] private GameObject ItemPrice;
+
+    public string itemName;
+    public int price;
+    public GameObject pickUpPrefab; // Prefab for the PickUpComponent
+
+    private PickUpComponent selectedPickUp;
+
+    void Start()
+    {
+        // Set the item name
+        TextMeshProUGUI nameTextComponent = ItemName.GetComponent<TextMeshProUGUI>();
+        if (nameTextComponent != null)
+        {
+            nameTextComponent.text = itemName;
+        }
+        else
+        {
+            Debug.LogError("TextMeshProUGUI component not found");
+        }
+
+        // Set the item price
+        TextMeshProUGUI priceTextComponent = ItemPrice.GetComponent<TextMeshProUGUI>();
+        if (priceTextComponent != null)
+        {
+            priceTextComponent.text = price.ToString();
+        }
+        else
+        {
+            Debug.LogError("TextMeshProUGUI component not found");
+        }
+
+        // Set the item image from the pickUpPrefab
+        PickUpComponent pickUpComponent = pickUpPrefab.GetComponent<PickUpComponent>();
+        if (pickUpComponent != null)
+        {
+            SpriteRenderer spriteRenderer = pickUpComponent.GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                ItemImage.GetComponent<Image>().sprite = spriteRenderer.sprite;
+            }
+            else
+            {
+                Debug.LogError("SpriteRenderer component not found in pickUpPrefab");
+            }
+        }
+        else
+        {
+            Debug.LogError("PickUpComponent not found on pickUpPrefab");
+        }
+    }
+
+    public void OnItemClick()
+    {
+        // Find the player and get their position
+        PlayerController playerController = FindObjectOfType<PlayerController>();
+        if (playerController != null)
+        {
+            Vector3 playerPosition = playerController.transform.position;
+
+            // Spawn the PickUpComponent at the player's position
+            GameObject pickUpObject = Instantiate(pickUpPrefab, playerPosition, Quaternion.identity);
+            selectedPickUp = pickUpObject.GetComponent<PickUpComponent>();
+
+            if (selectedPickUp == null)
+            {
+                Debug.LogError("PickUpComponent not found on the instantiated prefab");
+                return;
+            }
+
+            // Notify the PlayerController to pick it up
+            playerController.SetSelectedPickUp(selectedPickUp);
+        }
+        else
+        {
+            Debug.LogError("PlayerController not found in the scene");
+        }
+    }
+}
