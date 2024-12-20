622ccfcd44cae692ac4fef3b33fbe91255ac9e85
nhatphong5954 <pukinanasi27@gmail.com>
2024-12-20 11:48:23 +0700

    Sửa Spike, thêm Enemy

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -3,35 +3,47 @@ using UnityEngine;
 public class Spike : MonoBehaviour
 {
     public float damage = 1;
-    public float changedTime = 2.0f;
+    public float changedTime = 2.5f;
     private float hitTimer = 0f;
-    [SerializeField]
-    private bool hit = true;
+    private bool hit = false;
     private Animator animator;
-
+    private bool aniState = true;
+    private bool hitCooldown = false;
+    private float hitCooldownTime = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = this.GetComponent<Animator>();
-        animator.SetBool("Hit", hit);
+        animator.SetBool("Hit", aniState);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         hitTimer += Time.deltaTime; 
         if (hitTimer >= changedTime)
         {
-            hit = !hit;
             hitTimer = 0f; 
-            animator.SetBool("Hit", hit);
+            aniState = !aniState;
+            animator.SetBool("Hit", aniState);
+        }
+
+        if (hitCooldown)
+        {
+            hitCooldownTime -= Time.deltaTime;
+            if (hitCooldownTime <= 0)
+            {
+                hitCooldownTime = 1f;
+                hitCooldown = false;
+            }
         }
         
     }
 
     void OnTriggerStay2D (Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player") && hit)
+        if (col.gameObject.CompareTag("Player") && hit && !hitCooldown)
             {
                 IDamageable damageable = col.gameObject.GetComponent<IDamageable>();
                 BaseCharacter target = col.gameObject.GetComponent<BaseCharacter>();
@@ -45,7 +57,14 @@ public class Spike : MonoBehaviour
                     damageData.SourcePosition = this.transform.position;
                     damageData.TargetPosition = target.transform.position;
                     damageable.TakeDamage(damageData);
+                    hitCooldown = true;
                 }
             }
     }
+
+    private void switchHit()
+    {
+        hit = !hit;
+    }
+
 }
