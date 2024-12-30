dd43c85e108cd82d830b3113922d7800e493001f
lhson0606 <lehoangson01633892497@gmail.com>
2024-12-31 04:00:10 +0700

    add damge to BlazeRing

--- Assets/Scripts/Secondaries/BlazeRing.cs
+++ Assets/Scripts/Secondaries/BlazeRing.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class BlazeRing : BaseWeapon
 {
-
+    public override void OnSetOwner(BaseCharacter owner)
+    {
+        // get all WispFire components in children
+        WispFire[] wispFires = GetComponentsInChildren<WispFire>();
+        foreach (WispFire wispFire in wispFires)
+        {
+            wispFire.SetOwner(owner);
+        }
+    }
 }
