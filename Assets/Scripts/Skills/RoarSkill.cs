23da01cb4aa867c2bdd1161e957293054fbf171f
lhson0606 <lehoangson01633892497@gmail.com>
2025-01-18 09:13:57 +0700

    save

---
+++ Assets/Scripts/Skills/RoarSkill.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RoarSkill : BaseSkill
+{
+    [SerializeField]
+    private AudioClip roarSound;
+    private GlobalShockWaveController shockWaveController;
+
+    private void Awake()
+    {
+        shockWaveController = FindFirstObjectByType<GlobalShockWaveController>();
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+        shockWaveController.DoShockWave(owner.transform.position, 1f, 1f);
+        if(roarSound != null)
+            AudioUtils.PlayAudioClipAtPoint(roarSound, owner.transform.position);
+        owner.GetComponent<BaseCharacter>().Heal(64);
+        owner.IsTa
+    }
+}
